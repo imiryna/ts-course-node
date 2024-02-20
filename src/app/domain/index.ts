05c101fdb6a1f9db9ed8331bd2dacd5ccf7beea4
Iryna Burlaka <i.am.blinova@gmail.com>
2024-02-20 17:00:20 +0100

    Setup server

--- src/app/domain/index.ts
+++ src/app/domain/index.ts
@@ -1,3 +1,13 @@
 const controllers = <any>[];
 
 export { controllers };
+
+import Person from "./person/Person";
+
+// Додаємо тип Controller для дотримання типів контролерів
+type Controller = typeof Person;
+
+// Додаємо контролер Person у масив controllers
+const controllers = <Controller[]>[Person];
+
+export { controllers };
---
+++ src/helpers/ApiResponse.ts
@@ -0,0 +1,3 @@
+export class ApiResponse<T> {
+  constructor(public success: boolean, public data: T, public message?: string) {}
+}
