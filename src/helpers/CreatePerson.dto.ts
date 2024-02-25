0b28a3aeb2bba270a0ddaf5965d9a9f421cbd2a3
Iryna Burlaka <i.am.blinova@gmail.com>
2024-02-25 16:33:22 +0100

    controller

---
+++ src/helpers/CreatePerson.dto.ts
@@ -0,0 +1,17 @@
+import { IsEmail, Length, IsInt, Min, Max } from "class-validator";
+
+// import type { IPerson } from "./Person.types";
+import type { IPerson } from "../app/domain/person/Person.types";
+
+export class CreatePerson implements Omit<IPerson, "id"> {
+  @Length(2, 20)
+  name: string;
+
+  @IsEmail()
+  email: string;
+
+  @IsInt()
+  @Min(18)
+  @Max(99)
+  age: number;
+}
